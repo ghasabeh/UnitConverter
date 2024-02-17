f0924a441e865c4813a0a37334a7cf0b9b5895af
Alireza Ghasabeie <alireza.ghasabeie@devolon.fi>
2024-02-17 08:20:15 +0330

    add text field and a button and a toast whenever the button is clicked.

--- app/src/main/java/com/ghasabeie/unitconverter/MainActivity.kt
+++ app/src/main/java/com/ghasabeie/unitconverter/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.ghasabeie.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.ghasabeie.unitconverter.ui.theme.UnitConverterTheme
 
@@ -24,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    UnitConverter()
                 }
             }
         }
@@ -34,26 +38,27 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun UnitConverter() {
     Column {
-        Greeting("Alireza")
-        Row {
-            Greeting("Alireza")
-            Greeting("Alireza")
-        }
-    }
-}
+        Text("Unit Converter")
+        OutlinedTextField(value = "", onValueChange = {
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+        })
+        Row {
+            val context = LocalContext.current
+            Button(onClick = {
+                Toast.makeText(context,
+                    "Thanks for clicking",
+                    Toast.LENGTH_LONG
+                    ).show()
+            }) {
+                Text("Click Me!")
+            }
+        }
+        Text("Result: ")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    UnitConverterTheme {
-        UnitConverter()
-    }
+fun UnitConverterPreview() {
+    UnitConverter()
 }
