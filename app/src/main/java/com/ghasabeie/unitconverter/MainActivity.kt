8e397bdd7ab4789dd618f600eb7665fcb649675f
Alireza Ghasabeie <alireza.ghasabeie@devolon.fi>
2024-02-17 11:31:15 +0330

    Usage Box, Button and Icon, Modifier and vertical arrangement and horizontal alignment.

--- app/src/main/java/com/ghasabeie/unitconverter/MainActivity.kt
+++ app/src/main/java/com/ghasabeie/unitconverter/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.ghasabeie.unitconverter
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.ghasabeie.unitconverter.ui.theme.UnitConverterTheme
 
@@ -37,20 +41,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter() {
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text("Unit Converter")
         OutlinedTextField(value = "", onValueChange = {
 
         })
         Row {
-            val context = LocalContext.current
-            Button(onClick = {
-                Toast.makeText(context,
-                    "Thanks for clicking",
-                    Toast.LENGTH_LONG
-                    ).show()
-            }) {
-                Text("Click Me!")
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+
+                }
+            }
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+
+                }
             }
         }
         Text("Result: ")
