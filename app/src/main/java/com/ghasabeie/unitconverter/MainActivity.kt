f73691bac438ec67c9ed500993e7573e9ae0a1a8
Alireza Ghasabeie <alireza.ghasabeie@devolon.fi>
2024-02-17 15:29:44 +0330

    using spacer between components

--- app/src/main/java/com/ghasabeie/unitconverter/MainActivity.kt
+++ app/src/main/java/com/ghasabeie/unitconverter/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
@@ -20,6 +22,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ghasabeie.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,9 +50,11 @@ fun UnitConverter() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("Unit Converter")
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(value = "", onValueChange = {
 
         })
+        Spacer(modifier = Modifier.height(16.dp))
         Row {
             Box {
                 Button(onClick = { /*TODO*/ }) {
@@ -66,6 +71,7 @@ fun UnitConverter() {
                 }
             }
         }
+        Spacer(modifier = Modifier.height(16.dp))
         Text("Result: ")
     }
 }
